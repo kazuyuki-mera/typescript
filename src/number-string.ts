4f256ceedea165a0613a2e1484a62d7aa3306df9
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2022-01-12 22:29:05 +0900

    number型 string型

---
+++ src/number-string.ts
@@ -0,0 +1,12 @@
+export {};
+
+let year: number = 1995;
+// year = false;
+console.log({ year });
+
+let age: number = 26;
+console.log({ age });
+
+let name: string = 'kaz';
+// name = 10;
+console.log({ name });
