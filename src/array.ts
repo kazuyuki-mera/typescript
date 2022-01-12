83ca188edbc1b1d717694f32870bea65ca9cfbb4
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2022-01-12 22:38:43 +0900

    array型

---
+++ src/array.ts
@@ -0,0 +1,14 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+let strings: string[] = ['JavaScript', 'TypeScript', 'CoffeeScript'];
+
+let numbers2: Array<number> = [1, 2, 3];
+let strings2: Array<string> = ['Tokyo', 'Chiba'];
+
+let nijigenhairetsu: number[][] = [
+  [50, 100],
+  [150, 300],
+];
+
+let hairetsu: (number | string | boolean)[] = [1, 'abc', false];
