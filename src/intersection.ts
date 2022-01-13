da581efbc29789eb9946dc6f7ddf78addeaabc39
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2022-01-13 23:58:24 +0900

    intersection型

---
+++ src/intersection.ts
@@ -0,0 +1,29 @@
+export {};
+
+type Pitcher1 = {
+  throwingSpeed: number;
+};
+
+type Batter1 = {
+  battingAverage: number;
+};
+
+// type TwoWayPlayer {
+//   throwingSpeed: number;
+//   battingAverage: number;
+// }
+
+type TwoWayPlayer = Pitcher1 & Batter1;
+
+const sasaki: Pitcher1 = {
+  throwingSpeed: 154,
+};
+
+const ochiai: Batter1 = {
+  battingAverage: 0.367,
+};
+
+const ootani: TwoWayPlayer = {
+  throwingSpeed: 155,
+  battingAverage: 0.255,
+};
