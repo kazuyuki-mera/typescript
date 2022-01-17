dd06b39913d9a3053d41f9c98058b6f1a3cace93
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2022-01-17 21:49:12 +0900

    PickとOmit

---
+++ src/pick-and-omit.ts
@@ -0,0 +1,11 @@
+export {};
+
+type DetailedProfile = {
+  name: string;
+  height: number;
+  weight: number;
+};
+
+type SimpleProfile = Pick<DetailedProfile, 'name' | 'weight'>;
+
+type SmallProfile = Omit<DetailedProfile, 'height'>;
